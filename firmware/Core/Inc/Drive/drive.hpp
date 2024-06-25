3da5078bf98325bb117c208511c3a5d2f0bf2b3b
Peter Lilley <hi.petelilley@gmail.com>
2024-06-25 10:24:07 -0400

    Some fixes to encoder counting, started adding pid controllers to drive class, etc.

--- firmware/Core/Inc/Drive/drive.hpp
+++ firmware/Core/Inc/Drive/drive.hpp
@@ -1,32 +1,46 @@
 #pragma once
 
 #include "Basic/subsystem.hpp"
+#include "Math/pid_controller.hpp"
 
 #include <cstdint>
 
 class Drive : public SubsystemSingleton<Drive> {
 
+  uint16_t m_last_left_encoder_ticks  = 0;
+  uint16_t m_last_right_encoder_ticks = 0;
+
   struct {
-    float left_dist_mm = 0.f;
+    float left_dist_mm  = 0.f;
     float right_dist_mm = 0.f;
 
-    float left_vel_mmps = 0.f;
+    float left_vel_mmps  = 0.f;
     float right_vel_mmps = 0.f;
   } m_data;
+
+  PIDController m_translational_left_pid;
+  PIDController m_translational_right_pid;
+  PIDController m_angular_pid;
+
+public:
+  Drive();
 
 public:
   void process() override;
   void send_feedback() override;
 
-  void stop() { set_speed_dir(0, GPIO_PIN_RESET, 0, GPIO_PIN_RESET); };
+  void hard_stop() { set_speed_dir_raw(0, GPIO_PIN_RESET, 0, GPIO_PIN_RESET); };
 
-  // Set the speed of the motors.
-  // Values should be between -1.0 and 1.0.
-  void set_speed(float left_percent, float right_percent);
+  // Set the speed of the motors (percent of max speed).
+  // Values should range between [-1.0, 1.0].
+  void set_speed_raw(float left_percent, float right_percent);
+
+  // Set the speed of the motors (velocity in mm/s).
+  void set_speed(float left_mmps, float right_mmps);
 
 private:
-  void set_speed_dir(uint8_t left, GPIO_PinState left_dir, uint8_t right,
-                     GPIO_PinState right_dir);
+  void set_speed_dir_raw(uint8_t left, GPIO_PinState left_dir, uint8_t right,
+                         GPIO_PinState right_dir);
 
   void update_encoders();
 };
--- firmware/Core/Src/Drive/drive.cpp
+++ firmware/Core/Src/Drive/drive.cpp
@@ -18,6 +18,7 @@ extern TIM_HandleTypeDef htim2;     // Right encoder
 
 static constexpr int8_t PWM_PERIOD = 10; // The resolution of the PWM.
 
+// Encoder constants.
 static constexpr int8_t ENCODER_MAGNET_POLES = 6;
 static constexpr float GEAR_RATIO            = 20.f;
 static constexpr float ENCODER_TICKS_PER_ROTATION =
@@ -28,9 +29,16 @@ static constexpr float WHEEL_CIRCUMFERENCE_MM =
     (WHEEL_DIAMETER_MM * std::numbers::pi_v<float>);
 
 static constexpr float ENCODER_TICKS_PER_MM =
-    ENCODER_TICKS_PER_ROTATION / WHEEL_CIRCUMFERENCE_MM;
+    (ENCODER_TICKS_PER_ROTATION / WHEEL_CIRCUMFERENCE_MM);
+static constexpr float ENCODER_MM_PER_TICK = (1.f / ENCODER_TICKS_PER_MM);
 
-static constexpr float ENCODER_MM_PER_TICK = 1.f / ENCODER_TICKS_PER_MM;
+// PID constants.
+static constexpr float TRANSLATIONAL_KP = 0.1f;
+static constexpr float TRANSLATIONAL_KI = 0.0f;
+static constexpr float TRANSLATIONAL_KD = 0.0f;
+static constexpr float ANGULAR_KP       = 0.1f;
+static constexpr float ANGULAR_KI       = 0.0f;
+static constexpr float ANGULAR_KD       = 0.0f;
 
 //
 // Static variables.
@@ -44,25 +52,36 @@ static uint8_t s_pwm_pulse_left  = 0;
 // Drive functions.
 //
 
+Drive::Drive()
+  : m_translational_left_pid(TRANSLATIONAL_KP, TRANSLATIONAL_KI,
+                             TRANSLATIONAL_KD, ROBOT_UPDATE_PERIOD_S),
+    m_translational_right_pid(TRANSLATIONAL_KP, TRANSLATIONAL_KI,
+                              TRANSLATIONAL_KD, ROBOT_UPDATE_PERIOD_S),
+    m_angular_pid(ANGULAR_KP, ANGULAR_KI, ANGULAR_KD, ROBOT_UPDATE_PERIOD_S) {}
+
 void Drive::process() { update_encoders(); }
 
-void Drive::set_speed(float left_percent, float right_percent) {
+void Drive::set_speed_raw(float left_percent, float right_percent) {
   const int8_t left(left_percent * PWM_PERIOD);
   const int8_t right(right_percent * PWM_PERIOD);
 
-  const int8_t abs_left  = std::abs(left);
-  const int8_t abs_right = std::abs(right);
+  const bool left_dir  = std::signbit(left);
+  const bool right_dir = std::signbit(right);
 
-  const GPIO_PinState left_dir =
-      (abs_left / left) > 0 ? GPIO_PIN_RESET : GPIO_PIN_SET;
-  const GPIO_PinState right_dir =
-      (abs_right / right) > 0 ? GPIO_PIN_RESET : GPIO_PIN_SET;
+  const int8_t left_out  = left_dir ? -left : left;
+  const int8_t right_out = right_dir ? -right : right;
 
-  set_speed_dir(abs_left, left_dir, abs_right, right_dir);
+  const GPIO_PinState left_dir_pin  = static_cast<GPIO_PinState>(left_dir);
+  const GPIO_PinState right_dir_pin = static_cast<GPIO_PinState>(right_dir);
+
+  set_speed_dir_raw(left_out, left_dir_pin, right_out, right_dir_pin);
 }
 
-void Drive::set_speed_dir(uint8_t left, GPIO_PinState left_dir, uint8_t right,
-                          GPIO_PinState right_dir) {
+void Drive::set_speed(float left_mmps, float right_mmps) {}
+
+void Drive::set_speed_dir_raw(uint8_t left, GPIO_PinState left_dir,
+                              uint8_t right, GPIO_PinState right_dir) {
+
   HAL_GPIO_WritePin(MOTOR_LEFT_DIR_GPIO_Port, MOTOR_LEFT_DIR_Pin, left_dir);
   HAL_GPIO_WritePin(MOTOR_RIGHT_DIR_GPIO_Port, MOTOR_RIGHT_DIR_Pin, right_dir);
 
@@ -71,15 +90,35 @@ void Drive::set_speed_dir(uint8_t left, GPIO_PinState left_dir, uint8_t right,
 }
 
 void Drive::update_encoders() {
-  const int8_t left_delta_ticks  = hlptim1.Instance->CNT;
-  const int8_t right_delta_ticks = htim2.Instance->CNT / 2;
+  // Encoders are configured to overflow at 0xFFFF.
+  const uint16_t left_ticks  = hlptim1.Instance->CNT;
+  const uint16_t right_ticks = htim2.Instance->CNT / 2;
+
+  const auto calc_delta_ticks = [](const auto current, const auto last) {
+    int32_t delta_ticks = current - last;
+
+    const bool signbit            = std::signbit(delta_ticks);
+    const int32_t abs_delta_ticks = signbit ? -delta_ticks : delta_ticks;
+
+    // If the delta is big, there must have been an overflow.
+    if (abs_delta_ticks > INT16_MAX) {
+      // Add 0xFFFF when deep negative, subtract when positive.
+      delta_ticks += UINT16_MAX * (signbit ? 1 : -1);
+    }
+
+    return delta_ticks;
+  };
+
+  const int32_t left_delta_ticks =
+      calc_delta_ticks(left_ticks, m_last_left_encoder_ticks);
+  const int32_t right_delta_ticks =
+      calc_delta_ticks(right_ticks, m_last_right_encoder_ticks);
+
+  m_last_left_encoder_ticks  = left_ticks;
+  m_last_right_encoder_ticks = right_ticks;
 
   const float left_delta_mm  = left_delta_ticks * ENCODER_MM_PER_TICK;
   const float right_delta_mm = right_delta_ticks * ENCODER_MM_PER_TICK;
-
-  // Reset the encoders.
-  __HAL_LPTIM_RESET_COUNTER(&hlptim1);
-  __HAL_TIM_SET_COUNTER(&htim2, 0);
 
   // Update the distance.
   m_data.left_dist_mm += left_delta_mm;
