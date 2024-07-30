fa1cc87930dc5f45790ab57d9ccb2666075cf8ee
Peter Lilley <hi.petelilley@gmail.com>
2024-07-30 18:04:50 -0400

    Constants

---
+++ firmware/Core/Inc/Basic/constants.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+class Constants {
+public:
+  static constexpr float CELL_WIDTH_MM = 180.f;
+
+  static constexpr float ROBOT_LENGTH_MM = 100.f;
+  static constexpr float ROBOT_WIDTH_MM  = 70.f;
+
+  // Distance between the left and right wheels.
+  static constexpr float ROBOT_TRACK_WIDTH_MM = 50.4f;
+};
